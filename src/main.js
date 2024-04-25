1f9a466ca8ca7e693b70faf0401b689398ad8c0c
fdaniel13s <u20221c628@upc.edu.pe>
2024-04-25 17:00:56 -0500

    feat: add primevue components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
+import PrimeVue from "primevue/config";
+import Toolbar from "primevue/toolbar";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(PrimeVue, {ripple: true})
+    .component('pv-toolbar',Toolbar)
+    .mount('#app')
