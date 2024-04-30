52b2557abc99cedf3c46c585565c40de795fba51
fdaniel13s <u20221c628@upc.edu.pe>
2024-04-30 18:17:57 -0500

    feat(control-panel-page): add cards of routes about different components.

--- src/statics-control-management/pages/control-panel/components/control-panel-page.component.vue
+++ src/statics-control-management/pages/control-panel/components/control-panel-page.component.vue
@@ -6,8 +6,86 @@ export default {
 
 <template>
 
+  <div class="main-container">
+    <div class="left-container">
+      <pv-card class="justify-content-center mt-2">
+        <template #content>
+          <i class="pi pi-chart-bar icon-large"></i>
+        </template>
+        <template #footer>
+          <router-link to="/"  rel="noopener">
+            <pv-button class="button-green" label="Statical reports" />
+          </router-link>
+        </template>
+      </pv-card>
+
+      <pv-card class="justify-content-center mt-2">
+        <template #content>
+          <i class="pi pi-clipboard icon-large"></i>
+        </template>
+        <template #footer>
+          <router-link to="/"  rel="noopener">
+            <pv-button class="button-green" label="Crop Registration" />
+          </router-link>
+        </template>
+      </pv-card>
+
+      <pv-card class="justify-content-center mt-2">
+        <template #content>
+          <i class="pi pi-comments icon-large"></i>
+        </template>
+        <template #footer>
+          <router-link to="/"  rel="noopener">
+            <pv-button class="button-green" label="Consultation forum" />
+          </router-link>
+        </template>
+      </pv-card>
+
+      <pv-card class="justify-content-center mt-2">
+        <template #content>
+          <i class="pi pi-history icon-large"></i>
+        </template>
+        <template #footer>
+          <router-link to="/"  rel="noopener">
+            <pv-button class="button-green" label="Crop History" />
+          </router-link>
+        </template>
+      </pv-card>
+
+
+    </div>
+
+    <div class="right-container">
+      <p>Here the donut-graph</p>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
+.main-container {
+  display: flex;
+  margin-top:8em;
+}
+
+.left-container,
+.right-container {
+  flex: 1;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.icon-large {
+  font-size: 8em;
+  color: black;
+  margin-bottom: 0.1em;
+}
+
+.button-green {
+  background-color: var(--primary-main-green); /* Ajusta este valor al tono de verde que prefieras */
+  color: white; /* Esto cambiará el color del texto del botón a blanco */
+}
+
 
 </style>
