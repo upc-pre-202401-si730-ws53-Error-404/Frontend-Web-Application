250dcb3c36ae15ebe86bfef96689165c410849df
PaoloGuilen <u202124343@upc.edu.pe>
2024-05-02 18:04:52 -0500

    feat: changed env. routes

--- src/crops/sowing-statistics-reports/services/statistics-api.service.js
+++ src/crops/sowing-statistics-reports/services/statistics-api.service.js
@@ -4,11 +4,11 @@ import http from "../../../shared/services/http-common.js";
 export class StatisticsApiService {
 
     getAllSowings(){
-        return http.get('/sowings')
+        return http.get('/crops-db/sowings')
     }
 
     getAllCrops() {
-        return http.get('/crops');
+        return http.get('/crops-db/crops');
     }
 
 }
