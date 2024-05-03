417cd38376805df9569849ce43c6d063aa187139
jufer10 <fcarrasco023@gmail.com>
2024-05-03 06:26:09 -0500

    feat: crop-information.component.vue

---
+++ src/crops/crop-information/components/crop-information.component.vue
@@ -0,0 +1,23 @@
+<script>
+  import CropinfoToolbar from "./cropinfo-toolbar.vue";
+  import GeneralInformation from "./general-information.component.vue";
+
+  export default {
+    name: 'CropInformation',
+    components: {GeneralInformation, CropinfoToolbar},
+  }
+</script>
+
+<template>
+  <div class="container">
+    <cropinfo-toolbar class="toolbar"></cropinfo-toolbar>
+    <general-information></general-information>
+  </div>
+
+</template>
+
+<style scoped>
+  .toolbar {
+  margin-bottom: 100px;
+  }
+</style>
