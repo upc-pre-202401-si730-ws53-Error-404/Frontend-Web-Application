2b5a300862f0b030a29e8aec2360b2e336fe8a19
jufer10 <fcarrasco023@gmail.com>
2024-05-03 10:16:01 -0500

    feat: controls.component.vue

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import CropListAndRegistrationManagementComponent
     from "../crops/pages/crop-list-and-registration-management.component.vue";
 import GeneralInformationComponent from "../crops/crop-information/components/general-information.component.vue";
 import CropCareComponent from "../crops/crop-information/components/crop-care.component.vue";
+import ControlsComponent from "../crops/crop-information/components/controls.component.vue";
 
 const routes = [
     { path: '/sowing-statistics-reports', component: StaticalCardComponent },
@@ -13,7 +14,8 @@ const routes = [
     { path:'/crop-list-and-registration',component: CropListAndRegistrationManagementComponent},
     { path: '/', redirect: '/control-panel'},
     { path:'/general-information',component: GeneralInformationComponent},
-    { path:'/crop-care',component: CropCareComponent}
+    { path:'/crop-care',component: CropCareComponent},
+    { path:'/controls',component: ControlsComponent}
 ];
 
 const router = createRouter({
