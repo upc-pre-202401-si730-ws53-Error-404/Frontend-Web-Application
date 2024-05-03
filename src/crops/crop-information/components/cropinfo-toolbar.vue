af6645acdc0e798a89620bc5e080f8a8157e06c4
jufer10 <fcarrasco023@gmail.com>
2024-05-03 04:32:43 -0500

    feat: crop info toolbar

---
+++ src/crops/crop-information/components/cropinfo-toolbar.vue
@@ -0,0 +1,67 @@
+<script>
+  export default {
+    name: 'CropinfoToolbar',
+    data() {
+      return {
+        title: "Crop Information",
+        buttons: ["General Information", "Crop Care", "Controls", "Diseases or Pests", "Products Used"],
+        activeButton: 0
+      };
+    },
+    methods: {
+      selectButton(index) {
+        this.activeButton = index;
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title">{{ title }}</h1>
+    <div class="buttons">
+      <button
+          v-for="(button, index) in buttons"
+          :key="index"
+          :class="{ active: activeButton === index }"
+          @click="selectButton(index)"
+      >
+        {{ button }}
+      </button>
+    </div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+
+.title {
+  margin-bottom: 50px;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+button {
+  margin: 0 20px;
+  padding: 8px 16px;
+  border: none;
+  cursor: pointer;
+}
+
+button.active {
+  background-color: #005f40;
+  color: white;
+}
+.line{
+  width: 100%;
+  height: 1px;
+  background: black;
+}
+</style>
