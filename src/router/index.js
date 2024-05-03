838e65bec889c898409c2ae03499bc32c5856db7
PaoloGuilen <u202124343@upc.edu.pe>
2024-05-03 15:04:48 -0500

    fix: new thing

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,14 @@ import myQueriesComponent from "../forum/pages/my-queries.component.vue";
 import StaticalCardComponent from "../crops/components/crops-amount-stats-card.component.vue";
 import CropsStatisticsComponent from "../crops/pages/crops-statistics.component.vue";
 import ControlPanelPageComponent from "../public/pages/control-panel/components/control-panel-page.component.vue";
-import CropListAndRegistrationManagementComponent
-    from "../crops/pages/crop-list-and-registration-management.component.vue";
 import CropInformationManagementComponent from "../crops/pages/crop-information-management.component.vue";
 import CropListAndRegistrationManagementComponent from "../crops/pages/crop-list-and-registration-management.component.vue";
-import GeneralInformationComponent from "../crops/crop-information/components/general-information.component.vue";
-import CropCareComponent from "../crops/crop-information/components/crop-care.component.vue";
-import ControlsComponent from "../crops/crop-information/components/controls.component.vue";
-import DiseasesOrPestsComponent from "../crops/crop-information/components/diseases-or-pests.component.vue";
-import ProductsUsedComponent from "../crops/crop-information/components/products-used.component.vue";
+import GeneralInformationComponent from "../crops/components/general-information.component.vue";
+import CropCareComponent from "../crops/components/crop-care.component.vue";
+import DiseasesOrPestsComponent from "../crops/components/diseases-or-pests.component.vue";
+import ProductsUsedComponent from "../crops/components/products-used.component.vue";
 import HistoryTableComponent from "../crops/components/history-table.component.vue";
+import ControlsComponent from "../crops/components/controls.component.vue";
 
 
 const routes = [
